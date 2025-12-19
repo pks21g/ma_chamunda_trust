4b9a2b29cc61da34f138e4e127c01c8f2cde846f
pshar41 <pshar41@wgu.edu>
2025-12-20 03:11:16 +0530

    adding homepage

---
+++ pages/js-utils/header_util.js
@@ -0,0 +1,16 @@
+var ham = document.querySelector(".ham-menu");
+var nav_items = document.querySelector(".navigation-items");
+var lines = document.querySelectorAll(".lines");
+ham.addEventListener("click", () => {
+  nav_items.classList.toggle("active");
+  ham.classList.toggle("active");
+});
+
+var menu_icon = document.getElementById("menu-icon");
+menu_icon.addEventListener("click", () => {
+  if (menu_icon.src.includes("/assets/hamburger.svg")) {
+    menu_icon.src = "/assets/trishul.svg";
+  } else {
+    menu_icon.src = "/assets/hamburger.svg";
+  }
+});
