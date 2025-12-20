dc1c45c5d55a5d31e2e5d4419591342e7bd155e9
pshar41 <pshar41@wgu.edu>
2025-12-20 06:48:05 +0530

    about page wip

--- pages/js-utils/header_util.js
+++ pages/js-utils/header_util.js
@@ -9,7 +9,7 @@ ham.addEventListener("click", () => {
 var menu_icon = document.getElementById("menu-icon");
 menu_icon.addEventListener("click", () => {
   if (menu_icon.src.includes("./assets/hamburger.svg")) {
-    menu_icon.src = "./assets/trishul.svg";
+    menu_icon.src = "./assets/trisul.svg";
   } else {
     menu_icon.src = "./assets/hamburger.svg";
   }
