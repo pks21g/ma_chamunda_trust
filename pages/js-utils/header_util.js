5493619562f6bf4dc77838304f0d06ea09cfe7a3
pks21g <paulsmith414@yahoo.com>
2025-12-21 04:17:48 +0530

    fixed hamburgerMenu

--- pages/js-utils/header_util.js
+++ pages/js-utils/header_util.js
@@ -1,3 +1,4 @@
+
 var ham = document.querySelector(".ham-menu");
 var nav_items = document.querySelector(".navigation-items");
 var lines = document.querySelectorAll(".lines");
@@ -6,11 +7,3 @@ ham.addEventListener("click", () => {
   ham.classList.toggle("active");
 });
 
-var menu_icon = document.getElementById("menu-icon");
-menu_icon.addEventListener("click", () => {
-  if (menu_icon.src.includes("/assets/hamburger.svg")) {
-    menu_icon.src = "/assets/trisul.svg";
-  } else {
-    menu_icon.src = "/assets/hamburger.svg";
-  }
-});
