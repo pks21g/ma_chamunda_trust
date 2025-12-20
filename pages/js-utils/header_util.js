64e1d1c3ee89f3c7cbe895d8df0c3ab9013047cc
pshar41 <pshar41@wgu.edu>
2025-12-20 22:28:28 +0530

    wip about.html

--- pages/js-utils/header_util.js
+++ pages/js-utils/header_util.js
@@ -8,9 +8,9 @@ ham.addEventListener("click", () => {
 
 var menu_icon = document.getElementById("menu-icon");
 menu_icon.addEventListener("click", () => {
-  if (menu_icon.src.includes("./assets/hamburger.svg")) {
-    menu_icon.src = "./assets/trisul.svg";
+  if (menu_icon.src.includes("/assets/hamburger.svg")) {
+    menu_icon.src = "/assets/trisul.svg";
   } else {
-    menu_icon.src = "./assets/hamburger.svg";
+    menu_icon.src = "/assets/hamburger.svg";
   }
 });
