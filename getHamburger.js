48aa30eacf08879b0a029efd373a623baf4c4994
pks21g <paulsmith414@yahoo.com>
2025-12-21 14:22:38 +0530

    project restructured

--- getHamburger.js
+++
@@ -1,8 +0,0 @@
-var menu_icon = document.getElementById("menu-icon");
-menu_icon.addEventListener("click", () => {
-  if (menu_icon.src.includes("/assets/hamburger.svg")) {
-    menu_icon.src = "./assets/trisul.svg";
-  } else {
-    menu_icon.src = "./assets/hamburger.svg";
-  }
-});
---
+++ js-utils/getHamburger.js
@@ -0,0 +1,8 @@
+var menu_icon = document.getElementById("menu-icon");
+menu_icon.addEventListener("click", () => {
+  if (menu_icon.src.includes("assets/hamburger.svg")) {
+    menu_icon.src = "assets/trisul.svg";
+  } else {
+    menu_icon.src = "assets/hamburger.svg";
+  }
+});
